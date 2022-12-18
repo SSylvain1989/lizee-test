4fe18ea4f74f0cf4656de873d54b3cfad56710b9
SSylvain1989 <sylvain.sigonnez@gmail.com>
2022-12-18 17:10:42 +0100

    feat: add getStaticProps function on landing page and fetch only one product to type the data, add a revalidate of 2hours

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Hero from "./components/shared/hero/Hero"
 
-export default function Home() {
+export default function Home({ allProducts } : any) {
+  console.log(allProducts)
   return (
     <div>
       <Hero />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const fetchAllProducts = await fetch("https://lizee-test-dad-nextjs-admin.lizee.io/shop-api/taxon-products/by-slug/categorie-t-shirts?limit=1&page=1")
+  const allProducts = await fetchAllProducts.json()
+    console.log(allProducts)
+  return {
+    props: { allProducts },
+    revalidate: 7200,
+  }
 }
