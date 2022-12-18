ea81587d8e0e3a8670c0bcb14728d6fc0a5d4683
SSylvain1989 <sylvain.sigonnez@gmail.com>
2022-12-18 01:30:10 +0100

    feat: add button component, bottom section of hero section, style for date picker

--- pages/components/shared/datePickerRange/DatePickerRange.tsx
+++ pages/components/shared/datePickerRange/DatePickerRange.tsx
@@ -5,6 +5,7 @@ import { DatePicker } from "@mui/x-date-pickers"
 import TextField from "@mui/material/TextField"
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
 import Box from "@mui/material/Box"
+import CustomButton from "../CustomButton/CustomButton"
 
 export default function HomeHeader() {
   const [startDate, setStartDate] = useState<Dayjs | null>(dayjs(new Date()))
@@ -31,68 +32,88 @@ export default function HomeHeader() {
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Box display="flex" alignItems="center" flexDirection="row" width="250px" borderRadius={50}>
-        <Box width="175px" borderRadius={4}>
-          <DatePicker
-            InputAdornmentProps={{ style: { display: "none" } }}
-            open={openStartDate}
-            onOpen={() => setOpenStartDate(true)}
-            onClose={() => setOpenStartDate(false)}
-            value={startDate}
-            inputFormat="DD/MM/YYYY"
-            minDate={dayjs(new Date())}
-            onChange={onChangeStartDate}
-            renderInput={(params) => {
-              return (
-                <TextField
-                  variant="standard"
-                  sx={{
-                    svg: { color },
-                    input: { color },
-                    label: { color },
-                    backgroundColor: "white",
-                    borderRadius: "5px 0 0 5px ",
-                    padding : "4px 0 0 10px"
-                  }}
-                  {...params}
-                  onClick={() => setOpenStartDate(true)}
-                />
-              )
-            }}
-          />
+      <Box
+        display="flex"
+        alignItems="center"
+        flexDirection="row"
+        width="320px"
+        borderRadius={50}
+        justifyContent="space-between"
+        marginTop={3}
+      >
+        <Box
+          display="flex"
+          alignItems="center"
+          flexDirection="row"
+          width="250px"
+          borderRadius={50}
+        >
+          <Box width="110px" borderRadius={4}>
+            <DatePicker
+              InputAdornmentProps={{ style: { display: "none" } }}
+              open={openStartDate}
+              onOpen={() => setOpenStartDate(true)}
+              onClose={() => setOpenStartDate(false)}
+              value={startDate}
+              inputFormat="DD/MM/YYYY"
+              minDate={dayjs(new Date())}
+              onChange={onChangeStartDate}
+              renderInput={(params) => {
+                return (
+                  <TextField
+                    variant="standard"
+                    sx={{
+                      svg: { color },
+                      input: { color },
+                      label: { color },
+                      backgroundColor: "white",
+                      borderRadius: "5px 0 0 5px ",
+                      padding: "4px 0 0 10px",
+                    }}
+                    {...params}
+                    onClick={() => setOpenStartDate(true)}
+                  />
+                )
+              }}
+            />
+          </Box>
+          <Box width="130px" borderRadius={4}>
+            <DatePicker
+              InputAdornmentProps={{ position: "start" }}
+              open={openEndDate}
+              onOpen={() => setOpenEndDate(true)}
+              onClose={() => setOpenEndDate(false)}
+              minDate={startDate?.add(2, "day")}
+              value={endDate}
+              inputFormat="DD/MM/YYYY"
+              onChange={onChangeEndDate}
+              renderInput={(params) => {
+                return (
+                  <TextField
+                    variant="standard"
+                    sx={{
+                      svg: { color },
+                      input: { color },
+                      label: { color },
+                      backgroundColor: "white",
+                      borderRadius: "0 5px 5px 0",
+                      padding: "4px 5px 0 0",
+                    }}
+                    {...params}
+                    InputLabelProps={{
+                      shrink: true,
+                      color: "primary",
+                    }}
+                    onClick={() => setOpenEndDate(true)}
+                  />
+                )
+              }}
+            />
+          </Box>
         </Box>
-        <Box width="220px" borderRadius={4}>
-          <DatePicker
-            InputAdornmentProps={{ position: 'start'}}
-            open={openEndDate}
-            onOpen={() => setOpenEndDate(true)}
-            onClose={() => setOpenEndDate(false)}
-            minDate={startDate?.add(2, "day")}
-            value={endDate}
-            inputFormat="DD/MM/YYYY"
-            onChange={onChangeEndDate}
-            renderInput={(params) => {
-              return (
-                <TextField
-                  variant="standard"
-                  sx={{
-                    svg: { color },
-                    input: { color },
-                    label: { color },
-                    backgroundColor: "white",
-                    borderRadius: "0 5px 5px 0",
-                    padding : "4px 5px 0 0"
-                  }}
-                  {...params}
-                  InputLabelProps={{
-                    shrink: true,
-                    color: "primary",
-                  }}
-                  onClick={() => setOpenEndDate(true)}
-                />
-              )
-            }}
-          />
+
+        <Box p={0}>
+          <CustomButton color={"primary"} label="Start"/>
         </Box>
       </Box>
     </LocalizationProvider>
--- pages/components/shared/hero/Hero.tsx
+++ pages/components/shared/hero/Hero.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image"
 import Container from "@mui/material/Container"
-import styles from "./Hero.module.scss"
 import DatePickerRange from "../datePickerRange/DatePickerRange"
+import CustomButton from "../CustomButton/CustomButton"
+import Box from "@mui/material/Box"
+import styles from "./Hero.module.scss"
 
 export default function Home() {
   return (
@@ -21,6 +23,81 @@ export default function Home() {
           <p>Delivered when you need it.</p>
           <DatePickerRange />
         </div>
+        <Box display="flex" gap={2} marginTop={2}>
+          <CustomButton color={"secondary"} label="Go to shop" />
+          <CustomButton color={"primary"} label="See our pack" darkUi={true} />
+        </Box>
+
+        <Box display="flex" gap={2} marginTop={2}>
+          <Box
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+            style={{
+              border: "1px solid white",
+              color: "white",
+              borderRadius: "4px",
+              padding: "8px",
+              width: "90px",
+              height: "95px",
+            }}
+          >
+            <Image
+              src="/images/Icon_shipping.svg"
+              layout="fixed"
+              width={61}
+              height={32}
+              alt="Truck delivery with a clock on the back of the truck"
+            />
+            <p className={styles["small-info"]}>Delivery on time</p>
+          </Box>
+
+          <Box
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+            style={{
+              border: "1px solid white",
+              color: "white",
+              borderRadius: "4px",
+              padding: "8px",
+              width: "90px",
+              height: "95px",
+            }}
+          >
+            <Image
+              src="/images/Icon_wash.svg"
+              layout="fixed"
+              width={29}
+              height={32}
+              alt="Truck delivery with a clock on the back of the truck"
+            />
+            <p className={styles["small-info"]}>Hygiene and quality control</p>
+          </Box>
+
+          <Box
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+            style={{
+              border: "1px solid white",
+              color: "white",
+              borderRadius: "4px",
+              padding: "8px",
+              width: "90px",
+              height: "95px",
+            }}
+          >
+            <Image
+              src="/images/Icon_Boxes.svg"
+              layout="fixed"
+              width={33}
+              height={32}
+              alt="Truck delivery with a clock on the back of the truck"
+            />
+            <p className={styles["small-info"]}>Easy returns</p>
+          </Box>
+        </Box>
       </Container>
     </div>
   )
