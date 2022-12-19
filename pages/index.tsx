3fa86e9fa29d8c93a4c6124a39ba2dc1025d544d
SSylvain1989 <sylvain.sigonnez@gmail.com>
2022-12-19 19:44:49 +0100

    chore: add types for the api response

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import { Items } from "../utils/apiResponseTypes"
 import Hero from "./components/shared/hero/Hero"
 
-export default function Home({ allProducts } : any) {
-  console.log(allProducts)
+export default function Home({ items }: Items) {
+  console.log(items)
   return (
     <div>
       <Hero />
@@ -10,11 +11,13 @@ export default function Home({ allProducts } : any) {
 }
 
 export async function getStaticProps() {
-  const fetchAllProducts = await fetch("https://lizee-test-dad-nextjs-admin.lizee.io/shop-api/taxon-products/by-slug/categorie-t-shirts?limit=1&page=1")
+  const fetchAllProducts = await fetch(
+    "https://lizee-test-dad-nextjs-admin.lizee.io/shop-api/taxon-products/by-slug/categorie-t-shirts?limit=10&page=1"
+  )
   const allProducts = await fetchAllProducts.json()
-    console.log(allProducts)
+  console.log(allProducts)
   return {
-    props: { allProducts },
+    props: { items: allProducts.items },
     revalidate: 7200,
   }
 }
