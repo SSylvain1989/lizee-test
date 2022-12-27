5aef2217bc84b0bef709f492c9be71a206dbfbe0
Sylvain <62504188+SSylvain1989@users.noreply.github.com>
2022-12-27 12:03:32 +0100

    feat: add tests for customButton component (#6)
    * build: add all necessary package to add my test/ feat: all test are working, but i get a warning about reactDOM
    
    * chore: change the test for customButton for a functionnel one
    
    * chore: correct typo on functionnel test

---
+++ components/shared/CustomButton/CustomButton.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen, waitFor } from "@testing-library/react"
+import CustomButton from "./CustomButton"
+
+describe("Button", () => {
+  function getButton() {
+    return screen.getAllByRole("button")[0]
+  }
+
+  it("should be display a button", () => {
+    render(<CustomButton color={"primary"} label={""} />)
+    const button = getButton()
+    expect(button).toBeInTheDocument()
+  })
+  it("should not have style 'background-color : transparent' if button is darkUi={false} and color='primary'", async () => {
+    render(<CustomButton darkUi={false} color="primary" label={""} />)
+    const button = getButton()
+    await waitFor(() => {
+      expect(button).not.toHaveStyle("background-color: transparent")
+    })
+  })
+  it("should have style 'background-color : transparent' if button is darkUi={true} and color='primary'", async () => {
+    render(<CustomButton darkUi={true} color="primary" label={""} />)
+    const button = getButton()
+    await waitFor(() => {
+      expect(button).toHaveStyle("background-color: transparent")
+    })
+  })
+  it("should have style 'background-color : #c2185b' if button is darkUi={false} and color='primary'", async () => {
+    render(<CustomButton darkUi={false} color="primary" label={""} />)
+    const button = getButton()
+    await waitFor(() => {
+      expect(button).toHaveStyle("background-color: #c2185b")
+    })
+  })
+  it("should not have style 'background-color : #1a237e' if button is darkUi={false} and color='primary'", async () => {
+    render(<CustomButton darkUi={false} color="primary" label={""} />)
+    const button = getButton()
+    await waitFor(() => {
+      expect(button).not.toHaveStyle("background-color: #1a237e")
+    })
+  })
+})
+
