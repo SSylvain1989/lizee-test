685c6e7d4e58f2144de3281c9375de5f7c8f9722
SSylvain1989 <sylvain.sigonnez@gmail.com>
2022-12-27 11:30:54 +0100

    chore: change the test for customButton for a functionnel one

--- components/shared/CustomButton/CustomButton.test.tsx
+++ components/shared/CustomButton/CustomButton.test.tsx
@@ -1,32 +1,43 @@
-import { render, unmountComponentAtNode } from "react-dom"
+import { render, screen, waitFor } from "@testing-library/react"
 import CustomButton from "./CustomButton"
 
-let container: HTMLDivElement
+describe("Button", () => {
+  function getButton() {
+    return screen.getAllByRole("button")[0]
+  }
 
-beforeEach(() => {
-  container = document.createElement("div")
-  document.body.appendChild(container)
+  it("should be display a button", () => {
+    render(<CustomButton color={"primary"} label={""} />)
+    const button = getButton()
+    expect(button).toBeInTheDocument()
+  })
+  it("should not have style 'background-color : transparent' if button is darkUi={false} and color='primary'", async () => {
+    render(<CustomButton darkUi={false} color="primary" label={""} />)
+    const button = getButton()
+    await waitFor(() => {
+      expect(button).not.toHaveStyle("background-color: transparent")
+    })
+  })
+  it("should have style 'background-color : transparent' if button is darkUi={false} and color='primary'", async () => {
+    render(<CustomButton darkUi={true} color="primary" label={""} />)
+    const button = getButton()
+    await waitFor(() => {
+      expect(button).toHaveStyle("background-color: transparent")
+    })
+  })
+  it("should have style 'background-color : #c2185b' if button is darkUi={false} and color='primary'", async () => {
+    render(<CustomButton darkUi={false} color="primary" label={""} />)
+    const button = getButton()
+    await waitFor(() => {
+      expect(button).toHaveStyle("background-color: #c2185b")
+    })
+  })
+  it("should not have style 'background-color : #1a237e' if button is darkUi={false} and color='primary'", async () => {
+    render(<CustomButton darkUi={false} color="primary" label={""} />)
+    const button = getButton()
+    await waitFor(() => {
+      expect(button).not.toHaveStyle("background-color: #1a237e")
+    })
+  })
 })
 
-afterEach(() => {
-  unmountComponentAtNode(container)
-  container.remove()
-})
-
-it("calls the correct callback when clicked", () => {
-  const onClick = jest.fn()
-  render(
-    <CustomButton color="primary" label="Start" onClick={onClick} />,
-    container
-  )
-  const button = container.querySelector("button") as HTMLButtonElement
-  button.dispatchEvent(new MouseEvent("click", { bubbles: true }))
-  expect(onClick).toHaveBeenCalled()
-})
-
-it("displays the correct label when clicked", () => {
-  render(<CustomButton color="primary" label="Start" />, container)
-  const button = container.querySelector("button") as HTMLButtonElement
-  button.dispatchEvent(new MouseEvent("click", { bubbles: true }))
-  expect(button.textContent).toBe("Start")
-})
